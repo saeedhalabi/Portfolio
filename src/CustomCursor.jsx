824b236877a29a8ff321e5c590337f316c61ef83
Saeed Halabi <saeedhalabi80@gmail.com>
2025-03-02 10:56:56 +0200

    Bug Fix: Rendering cursor on touch devices

--- src/CustomCursor.jsx
+++ src/CustomCursor.jsx
@@ -2,15 +2,23 @@ import { useEffect, useState } from "react";
 
 const CustomCursor = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isTouchDevice, setIsTouchDevice] = useState(false);
 
   useEffect(() => {
+    // Check if the device is a touch device
+    setIsTouchDevice(window.matchMedia("(pointer: coarse)").matches);
+
     const updateCursor = e => {
-      setPosition({ x: e.clientX, y: e.clientY });
+      if (!isTouchDevice) {
+        setPosition({ x: e.clientX, y: e.clientY });
+      }
     };
 
     window.addEventListener("mousemove", updateCursor);
     return () => window.removeEventListener("mousemove", updateCursor);
-  }, []);
+  }, [isTouchDevice]);
+
+  if (isTouchDevice) return null; // Don't render the cursor on touch devices
 
   return (
     <div
